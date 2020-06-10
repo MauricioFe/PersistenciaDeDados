bdff3cf10f5feb83615453f199ccd577ee2da3f5
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-06-10 09:54:19 -0300

    Adição do sharedPreferences

--- app/src/main/java/com/example/persistenciadedados/SplashScreenActivity.java
+++ app/src/main/java/com/example/persistenciadedados/SplashScreenActivity.java
@@ -2,7 +2,10 @@ package com.example.persistenciadedados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        SharedPreferences preferences = getSharedPreferences("user_preferences", MODE_PRIVATE);
+        if (preferences.contains("ja_abriu_app")) {
+            mostrarMain();
+        } else {
+            adicionarPreferenceJaAbriu(preferences);
+            mostrarSplash();
+        }
+    }
+
+    private void mostrarSplash() {
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMain();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMain() {
+        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void adicionarPreferenceJaAbriu(SharedPreferences preferences){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("ja_abriu_app", true);
+        editor.commit();
     }
 }
